c13dee9d74cf98be7d4766229bf8d76e99ce8b45
chloechiaw <chloechia@gmail.com>
2024-03-09 13:20:35 -0800

    add files

---
+++ testEigen/useGeometry.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#include <Eigen/Core >
+#include <Eigen/Geometry >
+
+using namespace Eigen;
+
+int main(int argc, char, **argv) {
+    Matrix3d rotation_matrix = Matrix3d::Identity();
+    
+}
